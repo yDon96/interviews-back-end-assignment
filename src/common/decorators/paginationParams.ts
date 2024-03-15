e246f1f843d24ec2101096f62c2e384f333974dd
Youssef Donadeo <you.donadeo@icloud.com>
2024-03-15 10:17:22 +0100

    Create a PaginationParams decorator to extrapolate pagination request value

---
+++ src/common/decorators/paginationParams.ts
@@ -0,0 +1,35 @@
+import { BadRequestException, createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { Request } from 'express';
+
+export interface Pagination {
+  page: number;
+  limit: number;
+  size: number;
+  offset: number;
+}
+
+export const getPaginationParams = (page: number, size: number): Pagination => {
+  // check if page and size are valid
+  if (isNaN(page) || page < 0 || isNaN(size) || size < 0) {
+    throw new BadRequestException('Invalid pagination params');
+  }
+  // do not allow to fetch large slices of the dataset
+  if (size > 100) {
+    throw new BadRequestException('Invalid pagination params: Max size is 100');
+  }
+
+  // calculate pagination parameters
+  const limit = size;
+  const offset = page * limit;
+  return { page, limit, size, offset };
+};
+
+export const PaginationParams = createParamDecorator(
+  (data, ctx: ExecutionContext): Pagination => {
+    const req: Request = ctx.switchToHttp().getRequest();
+    const page = parseInt(req.query.page as string);
+    const size = parseInt(req.query.size as string);
+
+    return getPaginationParams(page, size);
+  },
+);
