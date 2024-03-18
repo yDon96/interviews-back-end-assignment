c42830c99c106c65a0154c2adba2547a3566b338
Youssef Donadeo <you.donadeo@icloud.com>
2024-03-18 14:05:55 +0100

    Create unit test for getProducts in category service and controller

--- test/__mock__/categoryMock.model.ts
+++ test/__mock__/categoryMock.model.ts
@@ -1,10 +1,14 @@
 import { MockModel } from './mockDB.model';
 import { ICategory } from '../../src/models/categories/interfaces/categories.interfaces';
-
+/* eslint-disable */
 export class CategoryMockModel extends MockModel<ICategory> {
   protected entityStub = {
     _id: 'string',
     name: 'string',
     elementsCount: 0,
   };
+
+  async findProductsBy(categoryId: string) {
+    return [];
+  }
 }
