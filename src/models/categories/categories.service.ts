f1daff82bb95839d7ddbdffabf709d681eafa337
Youssef Donadeo <you.donadeo@icloud.com>
2024-03-18 14:06:19 +0100

    Implement functions in category service and controller

--- src/models/categories/categories.service.ts
+++ src/models/categories/categories.service.ts
@@ -1,16 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ICategory } from './interfaces/categories.interfaces';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { TCategorySchema } from './entities/category.entities';
+import { Model, Types } from 'mongoose';
+import {
+  CategorySchemaStatics,
+  TCategorySchema,
+} from './entities/category.entities';
+import { IProduct } from '../products/interfaces/products.interfaces';
 
 @Injectable()
 export class CategoriesService {
   constructor(
     @InjectModel('categories')
-    private readonly categoryModel: Model<TCategorySchema>,
+    private readonly categoryModel: Model<TCategorySchema> &
+      CategorySchemaStatics,
   ) {}
   async findAllCategories(): Promise<ICategory[]> {
     return this.categoryModel.find().exec();
+  }
+
+  async findProductsBy(categoryId: string): Promise<IProduct[]> {
+    if (!Types.ObjectId.isValid(categoryId)) {
+      throw new BadRequestException('Invalid Category ID');
+    }
+
+    return this.categoryModel.findProductsBy(categoryId);
   }
 }
--- src/models/categories/entities/category.entities.ts
+++ src/models/categories/entities/category.entities.ts
@@ -1,11 +1,29 @@
 import { InferSchemaType, Schema, model, Model } from 'mongoose';
 import { ICategory } from '../interfaces/categories.interfaces';
 
-export const CategorySchema = new Schema<ICategory, Model<ICategory>>({
-  _id: String,
-  name: String,
-  elementsCount: Number,
-});
+export interface CategorySchemaStatics {
+  findProductsBy(categoryId: string): Promise<any>;
+}
+
+export const CategorySchema = new Schema<
+  ICategory,
+  Model<ICategory>,
+  CategorySchemaStatics
+>(
+  {
+    _id: String,
+    name: String,
+    elementsCount: Number,
+  },
+  {
+    statics: {
+      findProductsBy(categoryId: string) {
+        console.log(categoryId);
+        return [];
+      },
+    },
+  },
+);
 export type TCategorySchema = InferSchemaType<typeof CategorySchema>;
 
 export const CategoryModel = model('categories', CategorySchema);
